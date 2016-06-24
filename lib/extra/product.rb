d071a7cd98f2e8a251ea75e7da8e59892e66a760
Wesley Conde <wesleyskap@gmail.com>
2016-06-24 12:34:01 -0300

    fix(loads#Products): content_type to json

--- lib/extra/product.rb
+++ lib/extra/product.rb
@@ -1,7 +1,10 @@
 module Extra
   class Product < Base
     def self.create!(params)
-      post('loads/products', [params].flatten, content_type: :gzip) do |body, request, result|
+      post('loads/products', [params].flatten, content_type: :json) do |body, request, result|
+        @result = result
+        @request = request
+        @body = body
         yield body, request, result if block_given?
         new persisted: result.is_a?(Net::HTTPCreated)
       end
